e4e9d70740702dc5980bda10663d46b89b3f33a6
Freddy Drennan <drennanfreddy@gmail.com>
2019-09-03 19:48:45 -0600

    Update

--- main.R
+++ main.R
@@ -3,14 +3,24 @@ library(signlanguage)
 if (FALSE)
   install_venv()
 
+LOCAL_PYTHON_PATH = '/Users/digitalfirstmedia/.virtualenvs/r-tensorflow/bin/python'
+AWS_PYTHON_PATH = '/home/ubuntu/.virtualenvs/r-tensorflow/bin/python'
+
+if (file.exists('/Users/digitalfirstmedia/.virtualenvs/r-tensorflow/bin/python')) {
+  PYTHON_PATH = LOCAL_PYTHON_PATH
+} else {
+  PYTHON_PATH = AWS_PYTHON_PATH
+}
+
 use_python(
-  '/home/ubuntu/.virtualenvs/r-tensorflow/bin/python'
+  PYTHON_PATH
 )
 
 dir_name = 'videos'
-dir.create('temp')
 
-if (FALSE) {
+if(!dir.exists('temp')) dir.create('temp')
+
+if (TRUE) {
   map(
     c(
       'https://www.youtube.com/watch?v=a5BD8SjhPSg',
@@ -23,21 +33,20 @@ if (FALSE) {
   )
 
 }
+
 mpg_files <- file.path(dir_name, list.files(dir_name))
 
 mpg_data <-
   mpg_files[[1]] %>%
-  load.video(skip.to=0,
-             frames=4,
-             verbose = TRUE,
-             fps = NULL)
-
+  load.video(frames=10,
+             verbose = TRUE)
 
 n_images <- dim(mpg_data)[3]
+
 for(i in 1:n_images) {
   save.image(
     as.cimg(mpg_data[,,i,],dim = dim(mpg_data)[c(1, 2, 4)]),
-    paste0(tempfile(tmpdir = 'temp'), '.jpg')
+    file.path('temp', paste0('image', i, '.jpg'))
   )
 }
 
